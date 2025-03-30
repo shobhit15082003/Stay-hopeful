145b9aa2a1d7a93341debd8584ee2c49bc96b7d4
shobhit15082003 <shobhitbtech1508@gmail.com>
2025-03-31 00:24:11 +0530

    onclick things enables

--- src/components/Tables/tablesData.jsx
+++ src/components/Tables/tablesData.jsx
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import dummyData from "../../dummyData/dummyData";
 import './tablesData.css';
 
-const TablesData = ({ onTableSelect }) => {
+
+import { useDispatch } from "react-redux";
+import { setQuery } from "../../store/editorSlice"; // Importing the Redux action
+
+
+const TablesData = () => {
+  const dispatch = useDispatch();
   const [expanded, setExpanded] = useState({ gettingStarted: false, tables: false });
   const [hoveredTable, setHoveredTable] = useState(null);
   const [search, setSearch] = useState("");
@@ -23,7 +29,7 @@ const TablesData = ({ onTableSelect }) => {
   // Get table schema (columns & data types)
   const getTableSchema = (tableName) => {
     if (!dummyData[tableName] || dummyData[tableName].length === 0) return [];
-    
+
     const firstRow = dummyData[tableName][0]; // Get the first row as reference
     return Object.entries(firstRow).map(([key, value]) => ({
       column: key,
@@ -31,10 +37,17 @@ const TablesData = ({ onTableSelect }) => {
     }));
   };
 
+  // Handle table click (Set query in Redux)
+  const handleTableClick = (tableName) => {
+    const columns = getTableSchema(tableName).map((col) => col.column).join(", ");
+    const query = `SELECT ${columns} FROM ${tableName};`; // SQL query
+    dispatch(setQuery(query)); // Store query in Redux
+  };
+
   return (
     <div className="sidebar">
       {/* Main Heading */}
-      {/* <h2 className="sidebar-heading">Lets's Begin</h2> */}
+      <h2 className="sidebar-heading">Getting Started</h2>
 
       {/* Getting Started Dropdown */}
       <div className="dropdown">
@@ -72,7 +85,7 @@ const TablesData = ({ onTableSelect }) => {
                 <li
                   key={table}
                   className="table-item"
-                  onClick={() => onTableSelect(table)}
+                  onClick={() => handleTableClick(table)} // Set query on click
                   onMouseEnter={() => setHoveredTable(table)}
                   onMouseLeave={() => setHoveredTable(null)}
                 >
