f833e6ae0bae91adacd5c2e875a6eaf5edfc44a3
shobhit15082003 <shobhitbtech1508@gmail.com>
2025-03-31 19:38:07 +0530

    tests

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import {store} from './store/store';
+import reportWebVitals from './reportWebVitals';
+
+
+const handleWebVitals = (metric) => {
+  console.log(metric); // Logs the Web Vitals metrics to the console
+};
 
 
 
@@ -13,6 +19,6 @@ root.render(
     <App />
   </Provider>
 );
-
+reportWebVitals(handleWebVitals);
 
 
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,11 +1,13 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+// reportWebVitals.js
+
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getCLS(onPerfEntry); // Cumulative Layout Shift
+      getFID(onPerfEntry); // First Input Delay
+      getFCP(onPerfEntry); // First Contentful Paint
+      getLCP(onPerfEntry); // Largest Contentful Paint
+      getTTFB(onPerfEntry); // Time to First Byte
     });
   }
 };
